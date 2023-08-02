72481b3cf74a208407c86baaa720ddbe53074e7b
walackCode <91230518+walackCode@users.noreply.github.com>
2023-08-03 09:09:09 +1000

    Add files via upload

---
+++ Outline Shapes CDA.cs
@@ -0,0 +1,99 @@
+#region Using Directives
+using System;
+using System.Drawing;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PrecisionMining.Common.Design;
+using PrecisionMining.Common.Design.Data;
+using PrecisionMining.Common.Math;
+using PrecisionMining.Common.UI.Design;
+using PrecisionMining.Spry.UI.Design;
+using PrecisionMining.Common.Units;
+using PrecisionMining.Spry;
+using PrecisionMining.Spry.Util;
+using PrecisionMining.Spry.Data;
+using PrecisionMining.Spry.Design;
+using PrecisionMining.Spry.Scenarios;
+using PrecisionMining.Spry.Scenarios.Haulage;
+using PrecisionMining.Spry.Scenarios.Scheduling;
+using PrecisionMining.Spry.Scripting;
+using PrecisionMining.Spry.Spreadsheets;
+using PrecisionMining.Spry.Util.OptionsForm;
+using PrecisionMining.Spry.Util.UI;
+#endregion
+
+public partial class CuttinShapes
+{
+	static List<Polygon> selectedShapeBoundary = new List<Polygon>();
+	
+	[CustomDesignAction]
+	internal static CustomDesignAction CreateOutline()
+	{		
+        var customDesignAction = new CustomDesignAction("Create Outline", "", "Outlines", Array.Empty<Keys>());
+		customDesignAction.Visible = DesignButtonVisibility.Animation;
+		customDesignAction.SelectionPanelEnabled = true;
+		customDesignAction.SetupAction += (s,e) =>
+		{
+			customDesignAction.SetSelectMode(SelectMode.SelectElements);
+		};
+		customDesignAction.OptionsForm += (s,e) => 
+		{
+			var form = OptionsForm.Create("test");
+			var outputSelection = form.Options.AddLayerSelect("Output",false);
+			e.OptionsForm = form;
+		};
+		customDesignAction.ApplyAction += (s,e) =>
+		{
+			var inputs = customDesignAction.ActionInputs.Triangulations;
+			Layer outputLayer = (Layer) customDesignAction.ActionSettings[0].Value;
+			
+			var backupShapes = outputLayer.Shapes.ToList();
+       		var backupTexts = outputLayer.Texts.ToList();
+	        var backupTriangulations = outputLayer.Triangulations.ToList();
+	        customDesignAction.UndoAction += (s1,e1) =>
+	        {
+	            SetLayerData(outputLayer.FullName, backupShapes, backupTexts, backupTriangulations);
+	        };
+	
+			List<TriangleMesh> triangleList = new List<TriangleMesh>();
+			foreach(var tri in inputs)
+			{
+				triangleList.Add(tri.Data);
+			}
+			TriangleMesh triangleMesh = TriangleMesh.UnionSolids(triangleList).OutputMesh;
+			selectedShapeBoundary = triangleMesh.CreateSilhouette(Vector3D.UpVector, true);
+			var newShapes = new List<Shape>();
+			foreach(var poly in selectedShapeBoundary)
+			{
+				newShapes.Add(new Shape(poly));			
+			}
+			
+			var outputShapes = backupShapes.ToList();
+			outputShapes.AddRange(newShapes);
+	        customDesignAction.RedoAction += (s1,e1) =>
+	        {
+	            SetLayerData(outputLayer.FullName, outputShapes, backupTexts, backupTriangulations);
+	        };
+			
+		};
+		return customDesignAction;
+    }
+	
+	internal static void SetLayerData(string path, List<Shape> shapes, List<Text> texts, List<LayerTriangulation> triangulations)
+    {
+        var layer = Layer.GetOrCreate(path);
+        layer.Shapes.Clear();
+        layer.Texts.Clear();
+        layer.Triangulations.Clear();
+
+        foreach(var shape in shapes)
+            layer.Shapes.Add(shape);
+        foreach(var text in texts)
+            layer.Texts.Add(text);
+        foreach(var triangulation in triangulations)
+            layer.Triangulations.Add(triangulation);
+    }
+}
